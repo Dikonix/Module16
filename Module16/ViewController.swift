30ada1f731a2b0dbb87f82a230d6e84b598bae63
Diana Brik <di.brik@yandex.kz>
2023-07-13 10:59:33 +0600

    Initial commit

---
+++ Module16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Module16
+//
+//  Created by Diana Brik on 13.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
